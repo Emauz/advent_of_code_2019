85e9db3d2ccb7804735f380c2b0f692af922e810
Eric Moss <gmail@ericmoss.com>
2019-12-04 11:35:55 -0500

    Completed day 1 part 2

---
+++ 01/part2.c
@@ -0,0 +1,20 @@
+// rocket counter fuel upper thing
+// but this time it's part 2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int runningTotal = 0;
+    int mass;
+    while(scanf("%d", &mass) != EOF){
+        int fuel = (mass / 3) - 2;
+        runningTotal += fuel;
+        mass = fuel;
+        while((fuel=(mass/3)-2)>0){
+            runningTotal += fuel;
+            mass = fuel;
+        }
+    }
+    printf("Total fuel required: %d\n", runningTotal);
+}
